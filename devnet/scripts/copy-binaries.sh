6b39f3e5f7c64b10ded166f563b3033ab78d7723
Grant Zukel <grant.zukel@gmail.com>
2024-02-27 09:44:19 -0700

    removed duplicate information and removed docs as they are on the main docs site

--- devnet/scripts/copy-binaries.sh
+++
@@ -1,9 +0,0 @@
-rm -rf /usr/local/bin/zetacored
-cp ~/go/bin/zetacored  /usr/local/bin/
-rm -rf /usr/local/bin/zetaclientd
-cp ~/go/bin/zetaclientd  /usr/local/bin/
-
-rm -rf /usr/local/bin/zetaclientd
-cp ~/go/bin/zetaclientd  /usr/local/bin/
-rm -rf /usr/local/bin/zetacored
-cp ~/go/bin/zetacored  /usr/local/bin/
--- devnet/scripts/remove-chain-db-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-killall zetacored
-rm -rf ~/.zetacored/data/snapshots
-rm -rf ~/.zetacored/data/cs.wal
-rm -rf ~/.zetacored/data/*.db
-
